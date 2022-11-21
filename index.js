7fa0b27bf7a8f5a2265d6ce33b8b343398050633
pauline-njunguna <paulinenjunguna.w@gmail.com>
2022-11-21 14:07:43 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+{
+  let companyName = "Scuber";
+  const scuber="Scuber";
+}
+{
+    const companyName=("Scuber")
+    let mostProfitableNeighborhood=("Chelsea")
+    let chelsea = "Chelsea";
+}
+{
+    const companyName = "Scuber";
+    let companyCeo=("Susan Smith")
+    let susansmith=("Susan Smith")
+}
